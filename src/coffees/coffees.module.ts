0a70f4c20498054a8949dbda30dc8009a0a7f01a
Eric Vrabel <evrabel@virginia529.com>
2021-05-10 15:02:56 -0400

    Added coffee module and dtos

---
+++ src/coffees/coffees.module.ts
@@ -0,0 +1,6 @@
+import { CoffeesController } from './coffees.controller';
+import { CoffeesService } from './coffees.service';
+import { Module } from '@nestjs/common';
+
+@Module({ controllers: [CoffeesController], providers: [CoffeesService] })
+export class CoffeesModule {}
