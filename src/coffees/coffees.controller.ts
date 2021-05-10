1f13699cebc9ad285f177c46db1e5e0c5b3d3eaa
Eric Vrabel <evrabel@virginia529.com>
2021-05-10 11:55:25 -0400

    Added Get and Post to the coffees controller

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
-export class CoffeesController {}
+export class CoffeesController {
+  @Get()
+  findAll() {
+    return 'This action returns all coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    // Note that the following string uses the backtick instead of the apostrophe
+    return `This action returns #${id} coffees`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+}
