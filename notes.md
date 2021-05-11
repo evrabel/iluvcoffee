bd489fb5c9379a2c90e05badb85785c576fbd6e8
Eric Vrabel <evrabel@yahoo.com>
2021-05-10 20:07:38 -0400

    Added TypeORM

---
+++ notes.md
@@ -0,0 +1,27 @@
+https://learn.nestjs.com/courses/enrolled/591712
+
+Upgrading node according to these instructions
+
+https://nodejs.org/en/download/package-manager/#n
+
+Had to run the commands with sudo
+
+Installed nest globally
+
+sudo npm i -g @nestjs/cli
+
+When I pulled the code down from git I had to use the https url. SSH didn't work.
+
+Command to start the server npm run start:dev
+
+Downloaded and installed docker desktop
+
+https://hub.docker.com/editions/community/docker-ce-desktop-mac
+
+Docker start and stop commands:
+
+docker-compose up -d
+
+docker-compose down
+
+Postgres user: postgres
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,29 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity() // sql table name: coffee
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column('json', { nullable: true })
   flavors: string[];
 }
+
+/*
+Generated Coffee table in PostgreSQL Database
+
++-------------+--------------+----------------------------+
+|                          coffee                         |
++-------------+--------------+----------------------------+
+| id          | int(11)      | PRIMARY KEY AUTO_INCREMENT |
+| name   	    | varchar      |                            |
+| brand       | varchar      |                            |
+| flavors     | json         |                            |
++-------------+--------------+----------------------------+
+*/
